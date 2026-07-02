8d6642c68b3cd9c69c7e379c066d684a541a8934
adi9cap <nathaditya915@gmail.com>
2026-07-02 12:46:45 +0530

    calculating n factorial using loops(limited number only)

---
+++ Loops/n-factorial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int factorial = 1;
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+    cout << "the factorial is: " << factorial << endl;
+    return 0;
+}
