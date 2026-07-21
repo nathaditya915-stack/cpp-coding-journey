f5c6c71c439e8ee5fb995e6a8b745937da65ac69
Aditya Nath <nathaditya915@gmail.com>
2026-07-21 21:23:15 +0530

    Implement function to sum first n natural numbers

---
+++ functions.cpp/sum_of_first_natural_numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void sumOfNaturalNum (int n) {
+    int sum =0;
+
+    for (int i=0; i < n; i++) {
+        sum += i;
+    }
+    cout << "Sum of first " << n << " natural numbers is: " << sum << endl;
+}
+
+
+
+int main () {
+    sumOfNaturalNum(101);
+    sumOfNaturalNum(100);
+    return 0;
+}
