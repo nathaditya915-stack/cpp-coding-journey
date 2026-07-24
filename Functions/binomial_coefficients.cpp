711b729a0670e0e77e2f29e00fb84350a9e6edc9
Aditya Nath <nathaditya915@gmail.com>
2026-07-24 13:06:48 +0530

    Implement binomial coefficient calculation using functions

---
+++ Functions/binomial_coefficients.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    int fact = 1;
+
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    return fact;
+}
+
+int nCr (int n, int r) {
+    int fact_n=factorial(n);
+    int fact_r=factorial(r);
+    int fact_nmr=factorial(n-r);
+    return fact_n / (fact_r * fact_nmr);
+}
+
+int main () {
+    int n, r;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << "Enter r: ";
+    cin >> r;
+
+    if (r > n) {
+        cout << "Invalid input: r cannot be greater than n." << endl;
+        return 1;
+    }
+
+    int result = nCr(n, r);
+    cout << "C(" << n << ", " << r << ") = " << result << endl;
+
+    return 0;
+}
