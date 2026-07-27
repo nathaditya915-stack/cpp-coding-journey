611dc90d48ee9f75ef626d178acbe9dc7e9b6713
Aditya Nath <nathaditya915@gmail.com>
2026-07-27 12:57:52 +0530

    Update factorials.cpp

--- Functions/factorials.cpp
+++ Functions/factorials.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void fact(int n) {
-    int factorial = 1;
-    
+int factorial (int n){
+    int fact=1;
     for (int i = 1; i <= n; i++) {
-        factorial *= i;
+        fact *= i;
     }
-    cout << "Factorial of " << n << " is: " << factorial << endl;
+    return fact;
 }
 
-
-
 int main () {
-    fact(3);
-    fact(4);
-    fact(5);
-    fact(10);
-
+    int n; 
+     cout << "Enter a number: " << endl;
+    cin >> n;
+    cout << "The factorial of " << n << " is: " << factorial(n) << endl;
     return 0;
 }
